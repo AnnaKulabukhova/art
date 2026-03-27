e2f04071b3a42202c5294028293d7f6c329540fb
Osadchuk <kulabukhova1997@mail.ru>
2026-03-27 20:52:28 +0700

    added logo

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import styles from "./Header.module.scss";
 import clsx from "clsx";
 
 export const Header = () => {
   return (
     <header className={clsx("p-2", styles.header)}>
-      Мой хедер
+      <Image 
+      src={'/icons/logo/logo_light.svg'}
+      alt="logo"
+      width={92}
+      height={28}
+      />
     </header>
   );
 };
